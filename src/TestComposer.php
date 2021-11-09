8be21023ef9774d5fae8b511c0130d22352a2bf5
batianhu <610698057@qq.com>
2021-11-09 20:25:52 +0800

    霸天虎的第一个测试组件

---
+++ src/TestComposer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace firstComposer;
+
+class TestComposer
+{
+    public function showTime()
+    {
+        echo '当时霸天虎时间: ' . date('Y-m-d H:i:s');
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
